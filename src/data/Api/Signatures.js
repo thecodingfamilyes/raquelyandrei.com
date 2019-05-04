b630455adc4d870cd5ccf8c248806d4e0ae49161
Antonio Gázquez <piticonejo@gmail.com>
2019-05-04 14:48:55 +0200

    Update Api client

--- src/data/Api/Signatures.js
+++ src/data/Api/Signatures.js
@@ -1,13 +1,15 @@
-const host = process.env.API_HOST || 'http://localhost';
-const url = `${host}/api/signatures`;
+export default () => {
+    const host = process.env.API_HOST || 'http://localhost';
+    const url = `${host}/api/signatures`;
 
-export default () => ({
-    fetchSignatures: url,
-    addSignature: ({ name, email, msg }) => ({
-        addedSignature: {
-            url: url,
-            method: 'POST',
-            body: JSON.stringify({ name, email, message: msg }),
-        },
-    }),
-});
+    return {
+        fetchSignatures: url,
+        addSignature: ({ name, email, msg }) => ({
+            addedSignature: {
+                url: url,
+                method: 'POST',
+                body: JSON.stringify({ name, email, message: msg }),
+            },
+        }),
+    };
+};
